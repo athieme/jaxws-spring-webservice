8239aeb93cee8df4260fb4ecd87998c1d6c5ea56
Alex Thieme <athieme@athieme.com>
2013-08-20 14:10:42 -0700

    quick commit of test that uses RestTemplate to access WSDL after starting SOAP service

--- jaxws-spring-webservice-server/src/test/java/com/athieme/MockWebServiceTest.java
+++ jaxws-spring-webservice-server/src/test/java/com/athieme/MockWebServiceTest.java
@@ -3,10 +3,14 @@
  */
 package com.athieme;
 
+import junitx.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
 
 import javax.xml.ws.Endpoint;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author athieme
@@ -16,6 +20,17 @@ public class MockWebServiceTest {
     @Before
     public void before() {
         Endpoint.publish("http://localhost:9999/webservice/mock", new MockWebService());
+    }
+
+    @Test
+    public void wsdl() {
+        // HACK uses RestTemplate until I generate the SOAP client
+        final RestTemplate restTemplate = new RestTemplate();
+        final Map<String, String> vars = new HashMap<String, String>();
+        final String response = restTemplate.getForObject("http://localhost:9999/webservice/mock?wsdl", String.class, vars);
+        Assert.assertNotNull(response);
+        Assert.assertTrue(response.contains("<message name=\"reverse\">"));
+        Assert.assertTrue(response.contains("<message name=\"echo\">"));
     }
 
     @Test
